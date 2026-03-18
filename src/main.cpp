6fa91b4c851aeafba73ff7ca1cfcbfb8af4faf05
EvoiisUbu <athonly@gmail.com>
2026-03-17 19:57:45 -0700

    hello tetrahedron, and some other files

--- src/main.cpp
+++ src/main.cpp
@@ -46,70 +46,26 @@ int main(){
     // Note all values are within -1 to 1 range (normalized device coordinates)
     GLfloat vertices[] = {
         // Triangle 1       // vertex Colors
-        -0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f,    
-        -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,   
-        0.0f, 0.5f, 0.0f,  0.0f, 0.0f, 1.0f,
+        0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,    // A
+        -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,   // B
+        0.0f, 0.5f, 0.0f,  0.0f, 0.0f, 1.0f,    // C
         
         // Triangle 2
-        0.9f, 0.9f, 0.0f, 1.0f, 0.0f, 0.0f,
-        0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,
-        0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
+        0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,    // A
+        -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,   // B
+        0.0f, 0.0f, 0.7f,  1.0f, 1.0f, 1.0f,    // D
+
+        // Triangle 3
+        0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,    // A
+        0.0f, 0.5f, 0.0f,  0.0f, 0.0f, 1.0f,    // C
+        0.0f, 0.0f, 0.7f,  1.0f, 1.0f, 1.0f,    // D
+
+        // Triangle 4
+        -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,   // B
+        0.0f, 0.5f, 0.0f,  0.0f, 0.0f, 1.0f,    // C
+        0.0f, 0.0f, 0.7f,  1.0f, 1.0f, 1.0f,    // D
+        
     };
-
-    // GLfloat and float are interchangeable
-    // Second triangle data for multiple VBO/VAO
-    // float tri2_vertices[] = {
-    //     // Triangle 2
-        // 0.9f, 0.9f, 0.0f,
-        // 0.0f, 0.0f, 0.0f,
-        // 0.5f, -0.5f, 0.0f
-    // };
-
-    // Rectangle
-    // float vertices[] = {
-    //     0.5f,  0.5f, 0.0f,  // top right
-    //     0.5f, -0.5f, 0.0f,  // bottom right
-    //     -0.5f, -0.5f, 0.0f,  // bottom left
-    //     -0.5f,  0.5f, 0.0f   // top left 
-    // };
-
-    unsigned int indices[] = {  // note that we start from 0!
-        0, 1, 3,   // first triangle
-        1, 2, 3    // second triangle
-    };
-
-    // Texture
-    // float texCoords[] = {
-    //     0.0f, 0.0f,  // lower-left corner  
-    //     1.0f, 0.0f,  // lower-right corner
-    //     0.5f, 1.0f   // top-center corner
-    // };
-
-    // unsigned int texture;
-    // glGenTextures(1, &texture);
-    // glBindTexture(GL_TEXTURE_2D, texture);    
-    
-    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
-    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
-
-    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-    
-    // int width, height, nrChannels;
-    // unsigned char *data = stbi_load("wall.jpg", &width, &height, &nrChannels, 0);
-    // if (data)
-    // {
-    //     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-    //     glGenerateMipmap(GL_TEXTURE_2D);
-    // }
-    // else
-    // {
-    //     std::cout << "Failed to load texture" << std::endl;
-    // }
-
-    // // texture cleanup
-    // stbi_image_free(data);
 
     // Init Shader Program
     Shader shader_program(SHADER_DIR "vertex_shader.vs", SHADER_DIR "fragment_shader.fs");
@@ -166,7 +122,6 @@ int main(){
                 - offset to the first component of the first vertex attribute
                 - 0 for the position components
                 - below in the glVertexAttribPointer call for the color components you can see the offset is (3 * sizeof(float))
-    
     */
 
 
@@ -174,34 +129,19 @@ int main(){
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
     
-
-
-    // Triangle 2, with separate VAO & VBO
-    // unsigned int VAO_2;
-    // glGenVertexArrays(1, &VAO_2);
-    // glBindVertexArray(VAO_2);
-
-    // uint VBO_2;
-    // glGenBuffers(1, &VBO_2);
-    // glBindBuffer(GL_ARRAY_BUFFER, VBO_2);
-    // glBufferData(GL_ARRAY_BUFFER, sizeof(tri2_vertices), tri2_vertices, GL_STATIC_DRAW);
-    // glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    // glEnableVertexAttribArray(0);
-
-
-
-    // Rectangle
-    // unsigned int EBO;
-    // glGenBuffers(1, &EBO);
-    // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    // glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW); 
     
     // Wireframe Mode
     // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
     transformLoc = glGetUniformLocation(shader_program.ID, "transform");
 
+    glEnable(GL_DEPTH_TEST);
+
     while (!glfwWindowShouldClose(window))
     {
+        // Clear depth buffer on each loop
+        glClear(GL_DEPTH_BUFFER_BIT);
+        // glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
         processInput(window);
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
@@ -222,17 +162,7 @@ int main(){
         // Triangle
         glBindVertexArray(VAO);
         glBindBuffer(GL_ARRAY_BUFFER, VBO);
-        glDrawArrays(GL_TRIANGLES, 0, 6);
-        
-        // Triangle 2
-        // glBindVertexArray(VAO_2);
-        // glBindBuffer(GL_ARRAY_BUFFER, VBO_2);
-        // glDrawArrays(GL_TRIANGLES, 0, 3);
-        // Points
-        // glDrawArrays(GL_POINTS, 0, 3);
-        
-        // Rectangle
-        // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawArrays(GL_TRIANGLES, 0, 12);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
